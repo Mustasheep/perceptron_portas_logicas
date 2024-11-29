35bdd5d9c98e6b36912eaba0eee8f33615830803
Thiago de Assis <156924136+Mustasheep@users.noreply.github.com>
2024-11-29 19:09:37 -0300

    Add files via upload

---
+++ Perceptron_AND.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+entradas = np.array([[0,0],[0,1],[1,0],[1,1]])
+saidas = np.array([0,0,0,1])
+pesos = np.array([0.0,0.0])
+
+taxaAprendizado = 0.5
+
+def Soma(e,p):
+    return e.dot(p)
+
+s = Soma(entradas, pesos)
+
+def stepFunctions(soma):
+    if (soma >= 1):
+        return 1
+    return 0
+
+def calculoSaida(reg):
+    s = reg.dot(pesos)
+    return stepFunctions(s)
+
+def aprendeAtualiza():
+    erroTotal = 1
+    while (erroTotal != 0):
+        erroTotal = 0
+        for i in range(len(saidas)):
+            calcSaida = calculoSaida(np.array(entradas[i]))
+            erro = abs(saidas[i] - calcSaida)
+            erroTotal += erro
+            for j in range(len(pesos)):
+                pesos[j] = pesos[j] + (taxaAprendizado * entradas[i][j] * erro)
+                print("Pesos Atualiazados >> " + str(pesos[j]))
+        print("Total de Erros: " + str(erroTotal))
+        print("-----------------------------------")
+        
+aprendeAtualiza()
